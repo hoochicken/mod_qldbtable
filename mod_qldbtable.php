a56e80e43eb8700d42a4fc1cb0f626aa0eb6be5c
hoochicken <mareike.riegel@ql.de>
2023-07-15 00:14:06 +0200

    feat (entry) entry display along with default image works now

--- mod_qldbtable.php
+++ mod_qldbtable.php
@@ -16,38 +16,45 @@ require_once dirname(__FILE__).'/vendor/autoload.php';
 
 /** @var stdClass $module */
 /** @var \Joomla\Registry\Registry $params */
-$app = Factory::getApplication();
-$helper = new QldbtableHelper($module, $params, Factory::getContainer()->get(DatabaseInterface::class));
-$input = Factory::getApplication()->getInput();
 
-$displayEntry = $helper->checkDisplayEntry($input);
+try {
+    $app = Factory::getApplication();
+    $input = Factory::getApplication()->getInput();
+    $helper = new QldbtableHelper($module, $params, Factory::getContainer()->get(DatabaseInterface::class));
 
-if ($displayEntry) {
-    $ident = $input->getInt(QldbtableHelper::GETPARAM_ENTRYID, 0);
-    $entry = $helper->getEntry($ident);
-    $entry = $helper->setImageDefault($entry, $helper->getEntryColumnType(), $params->get('entry_image_default', ''));
-    $entryColumns = $helper->getEntryColumnLabels();
-}
-
-if (QldbtableHelper::DISPLAY_CARDS === $params->get('display')) {
-    $imageColumn = $params->get('cardImageColumn', '');
-    $labelColumn = $params->get('cardLabelColumn', '');
-    $cardCssClass = $params->get('cardCssClass', 'col-md-2');
-    if (empty($imageColumn)) {
-        $app->enqueueMessage('MOD_QLDBTABLE_MSG_SET_IMAGECOLUMN');
+    /* get data of single entry, if needed */
+    $displayEntry = $helper->checkDisplayEntry($input);
+    if ($displayEntry) {
+        $ident = $input->getInt(QldbtableHelper::GETPARAM_ENTRYID, 0);
+        $entry = $helper->getEntry($ident);
+        $entry = $helper->setImageDefault($entry, $helper->getEntryColumnType(), $params->get('entry_image_default', ''));
+        $entryColumns = $helper->getEntryColumnLabels();
     }
-    if (empty($labelColumn)) {
-        $app->enqueueMessage('MOD_QLDBTABLE_MSG_SET_LABELCOLUMN');
+
+    /* get data image for cards */
+    if (QldbtableHelper::DISPLAY_CARDS === $params->get('display')) {
+        $imageColumn = $params->get('cardImageColumn', '');
+        $labelColumn = $params->get('cardLabelColumn', '');
+        $cardCssClass = $params->get('cardCssClass', 'col-md-2');
+        if (empty($imageColumn)) {
+            $app->enqueueMessage('MOD_QLDBTABLE_MSG_SET_IMAGECOLUMN');
+        }
+        if (empty($labelColumn)) {
+            $app->enqueueMessage('MOD_QLDBTABLE_MSG_SET_LABELCOLUMN');
+        }
     }
-}
 
-$columns = $helper->getColumnLabels();
-$data = $helper->getData();
-if (QldbtableHelper::DISPLAY_TABLE === $params->get('display', QldbtableHelper::DISPLAY_DEFAULT)) {
-    $data = $helper->alterData($data, $helper->getColumnType());
-} elseif (QldbtableHelper::DISPLAY_CARDS === $params->get('display', QldbtableHelper::DISPLAY_DEFAULT) && (bool)$params->get('cardImageTag', true)) {
-    $data = $helper->alterData($data, [$params->get('cardImageColumn', '') => QldbtableHelper::TYPE_IMAGE], trim($params->get('cardImageDefault', '')));
-}
-$data = $helper->addLink($data, $params->get('linkText', 'Link'), $module->id, $params->get('linkIdent', 'id'));
+    /* get data of rows */
+    $columns = $helper->getColumnLabels();
+    $data = $helper->getData();
+    if (QldbtableHelper::DISPLAY_TABLE === $params->get('display', QldbtableHelper::DISPLAY_DEFAULT)) {
+        $data = $helper->alterData($data, $helper->getColumnType());
+    } elseif (QldbtableHelper::DISPLAY_CARDS === $params->get('display', QldbtableHelper::DISPLAY_DEFAULT) && (bool)$params->get('cardImageTag', true)) {
+        $data = $helper->alterData($data, [$params->get('cardImageColumn', '') => QldbtableHelper::TYPE_IMAGE], trim($params->get('cardImageDefault', '')));
+    }
+    $data = $helper->addLink($data, $params->get('linkText', 'Link'), $module->id, $params->get('linkIdent', 'id'));
 
-require JModuleHelper::getLayoutPath('mod_qldbtable', $params->get('layout', 'default'));
+    require JModuleHelper::getLayoutPath('mod_qldbtable', $params->get('layout', 'default'));
+} catch (Exception $e) {
+    $app->enqueueMessage(implode('<br >', [$e->getMessage(), $e->getFile(), $e->getLine()]));
+}
